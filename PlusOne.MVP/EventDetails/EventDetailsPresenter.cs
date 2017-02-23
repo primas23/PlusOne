f9c83df444c7e141d26c07a551e417c7a48b3a47
Petar Petrov <primas.petrov@gmail.com>
2017-02-23 21:45:26 +0200

    Added Evet details

--- PlusOne.MVP/EventDetails/EventDetailsPresenter.cs
+++ PlusOne.MVP/EventDetails/EventDetailsPresenter.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PlusOne.Services;
+﻿using PlusOne.Services;
 using WebFormsMvp;
 
 namespace PlusOne.MVP.EventDetails
